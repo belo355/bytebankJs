c85ffd61e9a36bb517a9e36577838cb52e76133d
belo355 <edilson.belo@live.com>
2021-07-09 01:00:53 -0300

    a

--- SistemaAuth.js
+++ SistemaAuth.js
@@ -1,5 +1,13 @@
 export class SistemaAuth {
+
   static login(autenticavel, senha) {
-    return autenticavel.autenticar(senha);
+    if(SistemaAuth.ehAuth(autenticavel)){
+      return autenticavel.autenticar(senha);
+    }
+      return false; 
+  }
+
+  static ehAuth(autenticavel){
+    return "autenticar" in autenticavel && autenticavel.autenticar instanceof Function; 
   }
 }
